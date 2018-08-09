8c9b0bc5bc0beaaade05d7d79d47a39eeaca0fa1
randlet <randle.taylor@gmail.com>
2018-08-09 11:50:34 -0400

    add explicit on_delete for migration for Django 2

--- metadata/migrations/0001_initial.py
+++ metadata/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=256, db_index=True)),
                 ('value', models.CharField(max_length=256, db_index=True)),
                 ('object_id', models.PositiveIntegerField()),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=models.CASCADE)),
             ],
         ),
     ]
